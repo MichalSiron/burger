32c264d54cc518586ad85257cbad06cfac250198
Michal Siron Mac <michal.siron@centrum.cz>
2018-11-13 18:54:35 +0100

    side menu and hiding of it

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,27 @@ import React from 'react';
 import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const sideDrawer = () => {
+const sideDrawer = (props) => {
+
+    let sidemenu = [classes.SideDrawer, classes.Close];
+    if (props.open){
+        sidemenu = [classes.SideDrawer, classes.Open];
+    }
 
     return(
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <>
+            <Backdrop show={props.open} clicked={props.closeHandler}/>
+            <div className={sidemenu.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
             </div>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+        </>
     );
 };
 
